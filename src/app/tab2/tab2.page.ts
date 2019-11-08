1c955eb2d50f2e7b4d3f660ede940b8ef634c917
Carlos Herrera <carherco@gmail.com>
2019-11-08 08:36:57 +0100

    ejemplo de ToastController

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
 import { Parque } from 'src/model/parque';
 import { ParquesService } from 'src/app/services/parques.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -15,7 +14,7 @@ export class Tab2Page {
   parque: Parque;
   mensaje: string;
 
-  constructor(private parquesService: ParquesService, private camara: Camera) {
+  constructor(private parquesService: ParquesService, private camara: Camera, private toastController: ToastController) {
     this.parque = {
       nombre: '',
       direccion: '',
@@ -24,9 +23,23 @@ export class Tab2Page {
   }
 
   guardar() {
+    // this.parquesService.post(this.parque).subscribe(
+    //   respuesta => this.mensaje = 'Parque guardado correctamente',
+    //   error => this.mensaje = 'Error al guardar el parque'
+    // );
+
     this.parquesService.post(this.parque).subscribe(
-      respuesta => this.mensaje = 'Parque guardado correctamente',
-      error => this.mensaje = 'Error al guardar el parque'
+      respuesta => this.presentToast('Parque guardado correctamente'),
+      error => this.presentToast('Error al guardar el parque')
     );
+  }
+
+  async presentToast(mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
   }
 }
